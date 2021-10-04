88a706614c55a475f1492085de805eb46694159c
Kurs3 <cgs.kurs003@gmail.com>
2021-10-04 15:14:25 +0200

    #4 die vierte

--- src/main/java/com/ats/simpletest2/SimpleTest2.java
+++ src/main/java/com/ats/simpletest2/SimpleTest2.java
@@ -3,6 +3,6 @@ package com.ats.simpletest2;
 public class SimpleTest2 {
 
     public static void main(String[] args) {
-        System.out.println("Simple Test2");
+        System.out.println("Simple Test2 & 3");
     }
 }
