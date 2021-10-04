3767976fedfa711c4eaf9d466ecdc4038780deaa
Kurs3 <cgs.kurs003@gmail.com>
2021-10-04 15:37:56 +0200

    #5 die sechste

---
+++ src/test/java/com/ats/simpletest2/SimpleTest2Test.java
@@ -0,0 +1,22 @@
+package com.ats.simpletest2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTest2Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
