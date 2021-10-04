4f86e2aa67f2ee3a35834034c0919850d47084b7
Kurs3 <cgs.kurs003@gmail.com>
2021-10-04 16:06:05 +0200

    #7 die siebente

--- src/main/java/com/ats/simpletest2/SimpleTest2.java
+++ src/main/java/com/ats/simpletest2/SimpleTest2.java
@@ -1,8 +1,11 @@
 package com.ats.simpletest2;
 
+import javax.swing.*;
+
 public class SimpleTest2 {
 
     public static void main(String[] args) {
-        System.out.println("Simple Test2 & 3");
+      System.out.println("Simple Test2 & 3");
+      JOptionPane.showMessageDialog(null, "Hello");
     }
 }
