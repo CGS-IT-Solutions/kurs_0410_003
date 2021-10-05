a3f064623c38488041394dd478b92691b30e8787
Kurs3 <cgs.kurs003@gmail.com>
2021-10-05 10:04:28 +0200

    #8 die achte

--- src/main/java/com/ats/simpletest2/SimpleTest2.java
+++ src/main/java/com/ats/simpletest2/SimpleTest2.java
@@ -1,11 +1,23 @@
 package com.ats.simpletest2;
 
 import javax.swing.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SimpleTest2 {
 
+    private static final Logger logger = LogManager.getLogger(SimpleTest2.class);
+
     public static void main(String[] args) {
-      System.out.println("Simple Test2 & 3");
-      JOptionPane.showMessageDialog(null, "Hello");
+        logger.debug("Hello from Log4j 2 - DEBUG");
+        logger.error("Hello form Log4J 2 - ERROR Log");
+        System.out.println("Simple Test2 & 3");
+        JOptionPane.showMessageDialog(null, "Hello");
     }
+
+    public static String echo(String input ) {
+        logger.debug("Hello from Log4j 2");
+        return input;
+    }
+
 }
--- src/test/java/com/ats/simpletest2/SimpleTest2Test.java
+++ src/test/java/com/ats/simpletest2/SimpleTest2Test.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class SimpleTest2Test {
 
+    /*
     @BeforeEach
     void setUp() {
     }
@@ -18,5 +22,33 @@ class SimpleTest2Test {
 
     @Test
     void main() {
+    }
+    */
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void init() {
+        System.out.println("@BeforeEach - executes before each test method in this class");
+    }
+
+    @Test
+    void testSingleSuccessTest() {
+        System.out.println("Success");
+    }
+
+    @Test
+    void testSimpleTestMain() {
+        SimpleTest2.main(null);
+    }
+
+
+    @Test
+    void echo() {
+        String input = SimpleTest2.echo("input");
+        assertEquals("input2", input);
     }
 }
